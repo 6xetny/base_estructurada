b57f4dc35414b7b98cd2da8fe90d43635e85da10
6xetny <b4sti4n321@gmail.com>
2026-08-21 16:46:10 -0400

    sisi

--- clase1/empresa.sql
+++ clase1/empresa.sql
@@ -3,3 +3,6 @@ CREATE DATABASE empresa;
 
 -- El comando USE se utiliza para seleccionar la base de datos
 USE empresa;
+
+-- El comando DROP DATABASE sirve para eliminar una base de datos. 
+DROP DATABASE empresa;
